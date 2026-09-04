0a17f31f55ae3db7bbcb6249248ee884be80483f
Agnihotram Chinmayanand <06chinmayanand@gmail.com>
2026-09-04 23:48:36 +0530

    Update conv1_hlstb.c

--- conv1_hlstb.c
+++ conv1_hlstb.c
@@ -4,40 +4,40 @@
 #define IN_H 64
 #define IN_W 64
 #define IN_C 3
+
 #define OUT_C 16
 #define K 3
 
+#define OUT_H (IN_H-K+1)
+#define OUT_W (IN_W-K+1)
+
 void conv1_hls(
-    int8_t input[IN_H][IN_W][IN_C],
-    int8_t weights[K][K][IN_C][OUT_C],
-    int8_t bias[OUT_C],
-    int32_t output[IN_H-K+1][IN_W-K+1][OUT_C]
+    int8_t *input,
+    int8_t *weights,
+    int8_t *bias,
+    int32_t *output
 );
 
 int main()
 {
-    static int8_t input[IN_H][IN_W][IN_C];
-    static int8_t weights[K][K][IN_C][OUT_C];
-    static int8_t bias[OUT_C];
-    static int32_t output[IN_H-K+1][IN_W-K+1][OUT_C];
 
-    for (int h=0; h<IN_H; h++)
-        for (int w=0; w<IN_W; w++)
-            for (int c=0; c<IN_C; c++)
-                input[h][w][c] = 1;
+    static int8_t input[64*64*3];
+    static int8_t weights[3*3*3*16];
+    static int8_t bias[16];
+    static int32_t output[62*62*16];
 
-    for (int kh=0; kh<K; kh++)
-        for (int kw=0; kw<K; kw++)
-            for (int ic=0; ic<IN_C; ic++)
-                for (int oc=0; oc<OUT_C; oc++)
-                    weights[kh][kw][ic][oc] = 1;
+    for(int i=0;i<64*64*3;i++)
+        input[i]=1;
 
-    for (int oc=0; oc<OUT_C; oc++)
-        bias[oc] = 0;
+    for(int i=0;i<3*3*3*16;i++)
+        weights[i]=1;
 
-    conv1_hls(input, weights, bias, output);
+    for(int i=0;i<16;i++)
+        bias[i]=0;
 
-    printf("Output = %d\n", output[0][0][0]);
+    conv1_hls(input,weights,bias,output);
+
+    printf("Output[0] = %d\n",output[0]);
 
     return 0;
 }
